4d7bd5b0eaa6f943b41aaa72e70dcde251c14f4b
Tarras Weir <tarras.weir@ezyvet.com>
2023-02-02 15:05:50 +1300

    Second Commit - Added templates and html files

---
+++ projects/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('projects/', views.projects, name="projects (Custom typed)"),
+    path('project/', views.project, name="Singular project"),
+    path('strproject/<str:words>', views.strproject, name="String used")
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+#These are the functions that are created to send an http response.
+#Request is a static variable with the request itself - it does not contain any usable data (as far as I know).
+#The second variable is one in which can be returned. The words are the extentions that are input onto the url.
+def projects(request):
+    return render(request, 'projects.html')
+def project(request):
+    return HttpResponse(f'You are on the project (no s) page.')
+def strproject(request, words):
+    return HttpResponse(f'You have successfully reached the strprojects page! using {words} as an ID.')
